7a22a96590d9b11a501c4db3e79b1f152541b866
Кирилл <kirillmarkis1996@gmail.com>
2025-01-06 16:49:40 +0300

    Initial commit

---
+++ src/test/java/ru/kamaev/calculator/CalculatorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kamaev.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
